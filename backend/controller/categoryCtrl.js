819cdd4740960dcce6d1fa685c40824b7e2822d4
umangfrendy <umang.t@frendy.in>
2021-12-07 10:38:48 +0530

    done backend

--- backend/controller/categoryCtrl.js
+++ backend/controller/categoryCtrl.js
@@ -18,12 +18,31 @@ createCategory: async (req, res) => {
 //get all category
 getAllCategorys: async (req, res) => {
   try {
-    const products = await Category.find().populate();
+    const category = await Category.find().populate();
 
     res.status(200).json({
       msg: "Read all data successfully",
-      products,
+      category,
     });
+  } catch (error) {
+    res.status(500).json({ msg: error.message });
+  }
+},
+// get single category
+getCategory: async (req, res) => {
+  try {
+    const category = await Category.findById(req.params.id);
+
+    if (!category) {
+      res.json({
+        msg: "category not found",
+      });
+    } else {
+      res.json({
+        msg: "category read successfully",
+        category,
+      });
+    }
   } catch (error) {
     res.status(500).json({ msg: error.message });
   }
--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -4,6 +4,12 @@ const categotyCtrl = require("../controller/categoryCtrl")
 
 // Route for create category
 router.post("/create", categotyCtrl.createCategory)
+
+// Route for read all category
 router.get("/readAll", categotyCtrl.getAllCategorys)
+
+// Route for read category
+router.get("/read/:id", categotyCtrl.getCategory)
+
 
 module.exports = router
